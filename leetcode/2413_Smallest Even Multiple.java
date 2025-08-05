291e0613bdc35e6301cb312038bc394f1cfcf24e
rohitgupta1006 <rgksj1006@gmail.com>
2025-08-05 08:03:39 +0530

    Create 2413_Smallest Even Multiple.java

---
+++ leetcode/2413_Smallest Even Multiple.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int smallestEvenMultiple(int n) {
+        for(int i=2;i<=2*n;i+=2)
+        {
+            if(i%2==0 && i%n==0)
+            {
+                return i;
+            }
+        }
+        return n;
+    }
+}
